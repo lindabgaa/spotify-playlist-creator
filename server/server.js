89c2500ad33f67dc56488a2a4e567ad4be2ca6f9
Linda BOUGAA <lindabgaa@Host-003.lan>
2024-11-01 21:01:58 +0100

    setup env var & more

--- server/server.js
+++ server/server.js
@@ -9,6 +9,25 @@ const corsOptions = {
   origin: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(",") : [],
 };
 
+(() => {
+  const missingEnv = [];
+
+  if (!process.env.SPOTIFY_CLIENT_ID) missingEnv.push("SPOTIFY_CLIENT_ID");
+  if (!process.env.SPOTIFY_CLIENT_SECRET) missingEnv.push("SPOTIFY_CLIENT_SECRET");
+  if (!process.env.SPOTIFY_REDIRECT_URI) missingEnv.push("SPOTIFY_REDIRECT_URI");
+
+  if (missingEnv.length > 0) {
+    console.error(
+      "Missing required environment variables: ",
+      missingVars.join(", "),
+      "\nPlease refer to the README.md file for instructions on setting up the project."
+    );
+    process.exit(1);
+  }
+
+  console.log("All set!");
+})();
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
